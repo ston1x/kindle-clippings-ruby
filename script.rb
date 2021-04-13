2c73d00904375b75da2348ce3d2e2dd70e386d80
Nicolai Stoianov <stoianovnk@gmail.com>
2021-04-13 17:10:28 +0300

    🎂 Initial commit

---
+++ script.rb
@@ -0,0 +1,33 @@
+require 'pry'
+require 'csv'
+
+PATH      = 'clippings.txt'
+SEPARATOR = '=========='.freeze
+
+file  = File.read(PATH)
+notes = file.split(SEPARATOR)
+
+result = notes.map do |note|
+  attributes = note.split("\n")
+  attributes.delete("\r")
+
+  next if attributes.empty?
+
+  title     = attributes[0].strip.gsub("\xEF\xBB\xBF", '')
+  text      = attributes[-1].strip
+  timestamp = attributes[1][/(?<=Added on).+/].strip
+
+  [title, text, timestamp]
+end.compact
+
+grouped = result.group_by { |note| note[0] }
+
+grouped.each do |book|
+  file_name = "#{book[0]}.csv"
+
+  CSV.open(file_name, 'w') do |csv|
+    book[1].each do |note|
+      csv << note
+    end
+  end
+end
